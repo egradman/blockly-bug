ea18810856b7dcf0a698891a62cd5e1213777904
Eric Gradman <egradman@Erics-MacBook-Pro.local>
2022-04-03 11:00:21 -0700

    blockly bug for forum

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # blockly-bug
 
+This is a minimal repository to demonstrate the serialization/deserialization bug.
+
+1. Prep this sample with `npm install`
+1. Run it with `npm run serve`
+1. On the web page, drag a block into the **upper** Blockly workspace.  
+1. Press the "transfer" button.  This will serialize the upper workspace to JSON and deserialize it in the lower workspace.
+1. Try to drag the block in the lower workspace, and observe the error in the console.
+
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,139 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="container">
+    <div>
+      <button @click="transfer">transfer</button>
+    </div>
+    <BlocklyComponent id="blockly1" :options="options" ref="blockly1"></BlocklyComponent>
+    <BlocklyComponent id="blockly2" :options="options" ref="blockly2"></BlocklyComponent>
   </div>
-  <router-view/>
 </template>
 
+<script>
+/**
+ * @license
+ * 
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Main Vue component that includes the Blockly component.
+ * @author samelh@google.com (Sam El-Husseini)
+ */
+
+import Blockly from 'blockly';
+import BlocklyComponent from './components/BlocklyComponent.vue'
+import './blocks/stocks';
+import './prompt';
+
+export default {
+  name: 'app',
+  components: {
+    BlocklyComponent
+  },
+  data(){
+    return {
+      code: '',
+      options: {
+        media: 'media/',
+        grid:
+          {
+            spacing: 25,
+            length: 3,
+            colour: '#ccc',
+            snap: true
+          },
+        toolbox:
+        `<xml>
+          <category name="Logic" colour="%{BKY_LOGIC_HUE}">
+            <block type="controls_if"></block>
+            <block type="logic_compare"></block>
+            <block type="logic_operation"></block>
+            <block type="logic_negate"></block>
+            <block type="logic_boolean"></block>
+          </category>
+          <category name="Loops" colour="%{BKY_LOOPS_HUE}">
+            <block type="controls_repeat_ext">
+              <value name="TIMES">
+                <block type="math_number">
+                  <field name="NUM">10</field>
+                </block>
+              </value>
+            </block>
+            <block type="controls_whileUntil"></block>
+          </category>
+          <category name="Math" colour="%{BKY_MATH_HUE}">
+            <block type="math_number">
+              <field name="NUM">123</field>
+            </block>
+            <block type="math_arithmetic"></block>
+            <block type="math_single"></block>
+          </category>
+          <category name="Text" colour="%{BKY_TEXTS_HUE}">
+            <block type="text"></block>
+            <block type="text_length"></block>
+            <block type="text_print"></block>
+          </category>
+          <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
+            </category>
+          <category name="Stocks" colour="%{BKY_LOOPS_HUE}">
+            <block type="stock_buy_simple"></block>
+            <block type="stock_buy_prog"></block>
+            <block type="stock_fetch_price"></block>
+          </category>
+        </xml>`
+      }
+    }
+  },
+  methods: {
+    transfer() {
+      let workspace1 = this.$refs.blockly1.workspace;
+      let workspace2 = this.$refs.blockly2.workspace;
+
+      let json = Blockly.serialization.workspaces.save(workspace1);
+      Blockly.serialization.workspaces.load(json, workspace2);
+    }
+  }
+}
+</script>
+
 <style>
+html, body {
+  margin: 0;
+  height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  position: relative;
+  height: 100%;
 }
 
-#nav {
-  padding: 30px;
+#container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#blockly1 {
+  flex-grow: 1;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#blockly2 {
+  flex-grow: 1;
 }
 </style>
